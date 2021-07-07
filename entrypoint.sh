0d8708f5f73d8a263cf7e35a938096467f6b4466
Sivert Bakken <sivertba@users.noreply.github.com>
2021-07-07 16:29:28 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,4 +24,38 @@ git config --global user.name "Check test"
 # Ignore workflow files (we may not touch them)
 git update-index --assume-unchanged .github/workflows/*
 
-source check.sh;
+make clean;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo make clean success
+else
+  echo make clean failed
+  return -1
+fi
+
+make;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo make success
+else
+  echo make failed
+  return -1
+fi
+
+make ARCH=arm;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo make arm success
+else
+  echo make arm failed
+  return -1
+fi
+
+make test;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo check test success
+else
+  echo check failed
+  return -1
+fi
