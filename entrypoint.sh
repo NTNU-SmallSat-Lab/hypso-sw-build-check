8fdc2024a452779939f54564e66e9fd4fefcd986
Sivert Bakken <sivertba@users.noreply.github.com>
2021-07-07 15:24:31 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,37 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+make clean;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo make clean success
+else
+  echo make clean failed
+  return -1
+fi
+
+make;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo make success
+else
+  echo make failed
+  return -1
+fi
+
+make ARCH=arm;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo make arm success
+else
+  echo make arm failed
+  return -1
+fi
+
+make test;
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo check test success
+else
+  echo check failed
+  return -1
+fi
