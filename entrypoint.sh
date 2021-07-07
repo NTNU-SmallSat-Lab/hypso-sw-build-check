632795066fd9fefb194ae113c0fd624faeaef286
Sivert Bakken <sivertba@users.noreply.github.com>
2021-07-07 16:15:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 
-make clean;
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo make clean success
-else
-  echo make clean failed
-  return -1
-fi
+#!/bin/bash
 
-make;
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo make success
-else
-  echo make failed
-  return -1
-fi
+set -eu
 
-make ARCH=arm;
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo make arm success
-else
-  echo make arm failed
-  return -1
-fi
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
-make test;
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo check test success
-else
-  echo check failed
-  return -1
-fi
+echo "## Initializing git repo..."
+git init
+echo "### Adding git remote..."
+git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+echo "### Getting branch"
+BRANCH=${GITHUB_REF#*refs/heads/}
+echo "### git fetch $BRANCH ..."
+git fetch origin $BRANCH
+echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+git checkout $BRANCH
+
+echo "## Configuring git author..."
+git config --global user.email "CI-check@4hypso.no"
+git config --global user.name "Check test"
+
+# Ignore workflow files (we may not touch them)
+git update-index --assume-unchanged .github/workflows/*
+
+source check.sh;
