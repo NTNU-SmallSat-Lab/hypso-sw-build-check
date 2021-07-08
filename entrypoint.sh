f1d304b00990004982b9816c88944312194c4a91
sivertba <sivert.bakken@gmail.com>
2021-07-08 08:21:25 +0200

    Clean up docker action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,10 @@
-#!/bin/bash
-
-#!/bin/bash
-
-set -eu
-
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-
-echo "## Initializing git repo..."
-git init
-echo "### Adding git remote..."
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-echo "### Getting branch"
-BRANCH=${GITHUB_REF#*refs/heads/}
-echo "### git fetch $BRANCH ..."
-git fetch origin $BRANCH
-echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-git checkout $BRANCH
-echo "### Update Submodules"
-git submodule update --init
-
-echo "## Configuring git author..."
-git config --global user.email "CI-check@4hypso.no"
-git config --global user.name "Check test"
-
-# Ignore workflow files (we may not touch them)
-git update-index --assume-unchanged .github/workflows/*
-
 make clean;
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
   echo make clean success
 else
   echo make clean failed
-  return -1
+  exit 1
 fi
 
 make;
@@ -41,7 +13,7 @@ if [ $RESULT -eq 0 ]; then
   echo make success
 else
   echo make failed
-  return -1
+  exit 2
 fi
 
 make ARCH=arm;
@@ -50,7 +22,7 @@ if [ $RESULT -eq 0 ]; then
   echo make arm success
 else
   echo make arm failed
-  return -1
+  exit 3
 fi
 
 make test;
@@ -59,5 +31,5 @@ if [ $RESULT -eq 0 ]; then
   echo check test success
 else
   echo check failed
-  return -1
+  exit 4
 fi
